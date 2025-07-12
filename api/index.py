853a6ca8826f39327b7eebf1a1184ac278698b9f
Ikhsanarif <ikhsanarif211@gmail.com>
2025-07-12 14:05:25 +0700

    vercel deploy

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 
 @app.route('/')
